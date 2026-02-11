964fbbd371210392ead4ef03691991f89cb2d198
Benjaminakuetteh <Quophyjiro@gmail.com>
2026-02-11 17:01:57 +0000

    UI update

---
+++ Assets/Scripts/UI/SafeAreaFitter.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class SafeAreaFitter : MonoBehaviour
+{
+    private RectTransform _rt;
+    private Rect _lastSafe;
+
+    void Awake()
+    {
+        _rt = GetComponent<RectTransform>();
+        Apply();
+    }
+
+    void Update()
+    {
+        if (Screen.safeArea != _lastSafe) Apply();
+    }
+
+    private void Apply()
+    {
+        _lastSafe = Screen.safeArea;
+
+        var anchorMin = _lastSafe.position;
+        var anchorMax = _lastSafe.position + _lastSafe.size;
+
+        anchorMin.x /= Screen.width;
+        anchorMin.y /= Screen.height;
+        anchorMax.x /= Screen.width;
+        anchorMax.y /= Screen.height;
+
+        _rt.anchorMin = anchorMin;
+        _rt.anchorMax = anchorMax;
+        _rt.offsetMin = Vector2.zero;
+        _rt.offsetMax = Vector2.zero;
+    }
+}
